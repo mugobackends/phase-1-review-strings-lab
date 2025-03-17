32b457bcdd3258510709b23b09927da64b16a537
Lee <lee.thuku@student.moringaschool.com>
2025-03-17 09:28:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+
+const currentUser = 'Grace Hoppe'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWconst currentUser = 'Grace Hoppe'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;elcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
