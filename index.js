c3b892eaa66365c8ce3b4e1869d2296d0d6e21e4
Lee <lee.thuku@student.moringaschool.com>
2025-03-17 09:56:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-
-const currentUser = 'Grace Hoppe'
-
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
-const excitedWconst currentUser = 'Grace Hoppe'
+const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;elcomeMessage = welcomeMessage.toUpperCase();
-
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
